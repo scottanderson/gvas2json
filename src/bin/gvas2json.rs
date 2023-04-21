9c9eb61bd4b9c6b714e0b8466d7ee24bd780d810
Scott Anderson <scotta@gmail.com>
2023-04-21 03:11:19 -0400

    colored_json

--- src/bin/gvas2json.rs
+++ src/bin/gvas2json.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
-use clap::Parser;
+use clap::{Parser, ValueEnum};
+use colored_json::{ColorMode, Output};
 use gvas::GvasFile;
 use std::fs::File;
 use std::io::{BufReader, BufWriter, Cursor, Read, Write};
@@ -18,11 +19,41 @@ struct Args {
     /// Pretty-print JSON output.
     #[clap(short, long, id = "PRETTY", default_value_t = true)]
     pretty: bool,
+
+    /// Enable colored output.
+    #[clap(long, id = "COLOR", default_value = "auto")]
+    color: WhenValues,
+}
+
+#[derive(ValueEnum, Clone, Debug)]
+enum WhenValues {
+    /// Always show colored output.
+    Always,
+    /// Automatically detect if terminal supports colors.
+    Auto,
+    /// Never show colored output.
+    Never,
 }
 
 fn main() -> Result<()> {
     // Parse command-line arguments
     let args = Args::parse();
+
+    // Check for terminal color support
+    let color_mode = match args.color {
+        WhenValues::Always => ColorMode::On,
+        WhenValues::Auto => match args.output {
+            None => {
+                // Auto needs to be resolved before creating the pager
+                match ColorMode::Auto(Output::StdOut).use_color() {
+                    true => ColorMode::On,
+                    false => ColorMode::Off,
+                }
+            }
+            _ => ColorMode::Off,
+        },
+        WhenValues::Never => ColorMode::Off,
+    };
 
     // Read from input
     let gvas = match args.input {
@@ -31,16 +62,25 @@ fn main() -> Result<()> {
     }?;
 
     // Transcode the data
-    let json = match args.pretty {
-        true => serde_json::to_string_pretty(&gvas),
-        false => serde_json::to_string(&gvas),
-    }?;
+    let json = format_json(&gvas, color_mode, args.pretty)?;
 
     // Write to output
     match args.output {
         None => to_writer(std::io::stdout(), json.as_bytes()),
         Some(output) => to_writer(File::create(output)?, json.as_bytes()),
     }
+}
+
+fn format_json(gvas: &GvasFile, color_mode: ColorMode, pretty: bool) -> Result<String> {
+    let json = if color_mode.use_color() {
+        let value = serde_json::to_value(gvas)?;
+        colored_json::to_colored_json(&value, color_mode)?
+    } else if pretty {
+        serde_json::to_string_pretty(gvas)?
+    } else {
+        serde_json::to_string(gvas)?
+    };
+    Ok(json)
 }
 
 fn from_reader<R: Read>(input: R) -> Result<GvasFile> {
