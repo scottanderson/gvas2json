2a420eee9ba0d0c01866a74b842b4a877c932831
Scott Anderson <scottanderson@users.noreply.github.com>
2024-02-13 23:46:31 -0500

    gvas2yaml, yaml2gvas

--- tests/cli.rs
+++ tests/cli.rs
@@ -8,47 +8,38 @@ const ERROR_MISSING_FILE: &str = "Error: The system cannot find the path specifi
 #[cfg(not(windows))]
 const ERROR_MISSING_FILE: &str = "Error: No such file or directory (os error 2)\n";
 
-#[test]
-fn gvas2json_missing_file() {
-    Command::cargo_bin("gvas2json")
+fn missing_file(cmd: &str) {
+    Command::cargo_bin(cmd)
         .unwrap()
         .arg("test/file/doesnt/exist")
         .assert()
         .failure()
         .stderr(predicate::str::contains(ERROR_MISSING_FILE));
+}
+
+#[test]
+fn gvas2json_missing_file() {
+    missing_file("gvas2json")
+}
+
+#[test]
+fn gvas2yaml_missing_file() {
+    missing_file("gvas2yaml")
 }
 
 #[test]
 fn json2gvas_missing_file() {
-    Command::cargo_bin("json2gvas")
-        .unwrap()
-        .arg("test/file/doesnt/exist")
-        .assert()
-        .failure()
-        .stderr(predicate::str::contains(ERROR_MISSING_FILE));
+    missing_file("json2gvas")
 }
 
-fn gvas2json(case: &str) -> Result<()> {
-    Command::cargo_bin("gvas2json")?
-        .arg(&format!("resources/test/{case}.sav"))
-        .assert()
-        .success()
-        .stdout(fs::read_to_string(
-            [
-                env!("CARGO_MANIFEST_DIR"),
-                "resources",
-                "test",
-                &format!("{case}.json"),
-            ]
-            .iter()
-            .collect::<PathBuf>(),
-        )?);
-    Ok(())
+#[test]
+fn yaml2gvas_missing_file() {
+    missing_file("yaml2gvas")
 }
 
-fn json2gvas(case: &str) -> Result<()> {
-    Command::cargo_bin("json2gvas")?
-        .arg(&format!("resources/test/{case}.json"))
+fn bin(cmd: &str, case: &str, ext: &str, expect_ext: &str) -> Result<()> {
+    Command::cargo_bin(cmd)?
+        .arg(&format!("resources/test/{case}.{ext}"))
         .assert()
         .success()
         .stdout(fs::read(
@@ -56,12 +47,28 @@ fn json2gvas(case: &str) -> Result<()> {
                 env!("CARGO_MANIFEST_DIR"),
                 "resources",
                 "test",
-                &format!("{case}.sav"),
+                &format!("{case}.{expect_ext}"),
             ]
             .iter()
             .collect::<PathBuf>(),
         )?);
     Ok(())
+}
+
+fn gvas2json(case: &str) -> Result<()> {
+    bin("gvas2json", case, "sav", "json")
+}
+
+fn gvas2yaml(case: &str) -> Result<()> {
+    bin("gvas2yaml", case, "sav", "yaml")
+}
+
+fn json2gvas(case: &str) -> Result<()> {
+    bin("json2gvas", case, "json", "sav")
+}
+
+fn yaml2gvas(case: &str) -> Result<()> {
+    bin("yaml2gvas", case, "yaml", "sav")
 }
 
 #[test]
@@ -70,6 +77,16 @@ fn gvas2json_sample1() {
 }
 
 #[test]
+fn gvas2yaml_sample1() {
+    gvas2yaml("sample1").expect("gvas2yaml(sample1)");
+}
+
+#[test]
 fn json2gvas_sample1() {
     json2gvas("sample1").expect("json2gvas(sample1)");
+}
+
+#[test]
+fn yaml2gvas_sample1() {
+    yaml2gvas("sample1").expect("yaml2gvas(sample1)");
 }
